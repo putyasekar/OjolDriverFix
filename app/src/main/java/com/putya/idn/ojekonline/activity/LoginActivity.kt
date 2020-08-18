d9351a0c65f2402d9de4527fcad863d71121c223
Putya Sekar Ayu <5198258-putyasekar@users.noreply.gitlab.com>
2020-08-18 16:39:01 +0700

    login 1/2

--- app/src/main/java/com/putya/idn/ojekonline/activity/LoginActivity.kt
+++ app/src/main/java/com/putya/idn/ojekonline/activity/LoginActivity.kt
@@ -1,12 +1,67 @@
 package com.putya.idn.ojekonline.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.putya.idn.ojekonline.R
 
 class LoginActivity : AppCompatActivity() {
+    var googleSignInClient: GoogleSignInClient? = null
+    private var auth: FirebaseAuth? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+    private fun signIn() {
+        val gson = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gson)
+
+        val signInIntent = googleSignInClient?.signInIntent
+        startActivityForResult(signInIntent, 4)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 4) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account)
+            } catch (e: ApiException) {
+
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(account: GoogleSignInAccount?) {
+        var uid = String()
+        val credential = GoogleAuthProvider.getCredential(account?.idToken, null)
+
+        auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = auth?.currentUser
+                    checkDatabase(task.result?.user?.uid, account)
+                    uid = user?.uid.toString()
+                } else {
+
+                }
+            }
+    }
+
+    private fun checkDatabase(uid: String?, account: GoogleSignInAccount?) {
+
     }
 }
